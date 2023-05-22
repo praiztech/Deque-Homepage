b210019d1db5b3018946f64f33be4bb1a05426b6
praiztech <praisefagbabiaka@gmail.com>
2023-05-22 21:48:33 +0100

    Added more styling for mobile viewport

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,3 @@ document.getElementById('search-input').addEventListener('input', (evt) => {
   const searchInput = evt.target;
   searchInput.setAttribute('value', searchInput.value);
 });
-
-document.querySelector('.hamburger-btn').addEventListener('click', (evt) => {
-  const hamburgerButton = evt.currentTarget;
-  const navIsOpen = hamburgerButton.getAttribute('aria-expanded') === 'true';
-  hamburgerButton.setAttribute('aria-expanded', navIsOpen ? 'false' : 'true');
-  hamburgerButton.nextElementSibling.toggleAttribute('hidden');
-})
