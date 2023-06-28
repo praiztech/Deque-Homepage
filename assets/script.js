38fa1784913dcc7a7c8cf371768fca482e664eed
praiztech <praisefagbabiaka@gmail.com>
2023-06-28 17:57:36 +0100

    Fixed navigation button bug

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,43 @@
-const topLinks = document.getElementById('top-links');
+const primaryNav = document.getElementById('primary-nav');
 
 document.getElementById('search-input').addEventListener('input', (evt) => {
   const searchInput = evt.target;
   searchInput.setAttribute('value', searchInput.value);
 });
 
-topLinks.parentElement.querySelector('.hamburger-btn').addEventListener('click', (evt) => {
+primaryNav.firstElementChild.addEventListener('click', (evt) => {
   const target = evt.currentTarget;
-  toggleContent(target, target.getAttribute('aria-expanded') === 'true');
-});
-
-topLinks.parentElement.querySelector('.hamburger-btn').addEventListener('focusout', (evt) => {
-  const target = evt.currentTarget;
-  if (!topLinks.contains(evt.relatedTarget)) {
-    const openSubNavBtn = topLinks.querySelector('.main-subnav-btn[aria-expanded="true"]');
-    if (openSubNavBtn !== null) toggleContent(openSubNavBtn);
-    toggleContent(target);
+  const closeMainNav = target.getAttribute('aria-expanded') === 'true';
+  if (closeMainNav) {
+    const activatedSubNavBtn = primaryNav.lastElementChild.querySelector('.main-subnav-btn[aria-expanded="true"]');
+    if (activatedSubNavBtn !== null) toggleContent(activatedSubNavBtn);
   }
+  toggleContent(target, closeMainNav);
 });
 
-topLinks.querySelectorAll(':scope .main-subnav-btn').forEach((subNavBtn) => {
-  subNavBtn.addEventListener('click', (evt) => {
+primaryNav.lastElementChild.querySelectorAll(':scope .top-item').forEach((topNavItem) => {
+  topNavItem.firstElementChild.addEventListener('pointerdown', (evt) => {
+    evt.currentTarget.setPointerCapture(evt.pointerId);
+  });
+  topNavItem.firstElementChild.addEventListener('pointerup', (evt) => {
     const target = evt.currentTarget;
+    const activatedSubNavBtn = primaryNav.lastElementChild.querySelector('.main-subnav-btn[aria-expanded="true"]');
+    if (!(activatedSubNavBtn === null || activatedSubNavBtn === target)) toggleContent(activatedSubNavBtn);
     toggleContent(target, target.getAttribute('aria-expanded') === 'true');
   });
-});
-
-topLinks.querySelectorAll(':scope .top-item').forEach((topNavItem) => {
   topNavItem.addEventListener('focusout', (evt) => {
     const target = evt.currentTarget;
     if (!target.contains(evt.relatedTarget)) toggleContent(target.firstElementChild);
-  })
+  });
 });
+
+primaryNav.addEventListener('focusout', (evt) => {
+  if (primaryNav.contains(evt.relatedTarget)) return;
+  const activatedSubNavBtn = primaryNav.lastElementChild.querySelector('.main-subnav-btn[aria-expanded="true"]');
+  const activatedHamburgerBtn = primaryNav.querySelector('.hamburger-btn[aria-expanded="true"]');
+  if (activatedSubNavBtn !== null) toggleContent(activatedSubNavBtn);
+  if (activatedHamburgerBtn !== null) toggleContent(activatedHamburgerBtn);
+})
 
 document.querySelectorAll('.accordion button').forEach((accordionBtn) => {
   accordionBtn.addEventListener('click', (evt) => {
@@ -47,22 +53,31 @@ document.querySelectorAll('.accordion [id^="panel"]').forEach((accordionPanel) =
 });
 
 document.addEventListener('keydown', (evt) => {
-  if (evt.key !== 'Escape') return;
-  // check if a sub-nav btn is activated
-  let activatedNavBtn = topLinks.querySelector('.main-subnav-btn[aria-expanded="true"]');
-  // check if hamburger-btn is activated
-  if (!window.matchMedia('(min-width: 48em)').matches && activatedNavBtn === null) {
-    activatedNavBtn = topLinks.parentElement.querySelector('.hamburger-btn[aria-expanded="true"]');
-  }
-  if (activatedNavBtn !== null) {
-    toggleContent(activatedNavBtn);
-    activatedNavBtn.focus();
+  switch (evt.key) {
+    case 'Escape':
+      // check if a main-sub-nav btn is activated
+      let activatedNavBtn = primaryNav.lastElementChild.querySelector('.main-subnav-btn[aria-expanded="true"]');
+      // else check if the hamburger-btn is activated
+      if (!window.matchMedia('(min-width: 48em)').matches && activatedNavBtn === null) {
+        activatedNavBtn = primaryNav.querySelector('.hamburger-btn[aria-expanded="true"]');
+      }
+      if (activatedNavBtn !== null) {
+        toggleContent(activatedNavBtn);
+        activatedNavBtn.focus();
+      }
+      break;
+    case ' ':
+    case 'Enter':
+      const target = evt.target;
+      if (target.classList.contains('main-subnav-btn')) {
+        toggleContent(target, target.getAttribute('aria-expanded') === 'true');
+      }
+      break;
   }
 });
 
 document.addEventListener('scroll', () => {
   if (!window.matchMedia('(min-width: 48em)').matches) return;
-  const primaryNav = document.getElementById('primary-nav');
   if (primaryNav.getBoundingClientRect().top === 0 && !primaryNav.classList.contains('sticky')) {
     primaryNav.classList.add('sticky');
   } else if (primaryNav.getBoundingClientRect().top > 0 && primaryNav.classList.contains('sticky')) {
